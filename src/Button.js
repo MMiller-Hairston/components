2c0d3a61a804d9c7c9614d56eacf87cead8689c7
Michael Miller <mjmill22@asu.edu>
2019-01-14 19:10:51 -0700

    init commit

---
+++ src/Button.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+/*
+    1. Create a functional component Named Button.
+    2. Make the button console.log hello when pressed. 
+    (put the console log inside a function so you can test it.)
+    3. Add a disabled prop to toggle if the button is active or not.
+*/
